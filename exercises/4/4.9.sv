fe4e1218ff3bd0027ddb2200e982088ae19267a8
Marcus Mellor <infinitymdm@gmail.com>
2024-01-05 13:37:53 -0600

    Remove textbook exercises

--- exercises/4/4.9.sv
+++
@@ -1,8 +0,0 @@
-// Exercise 4.9
-// Write a structural module to compute y = a & ~b | ~b & ~c | ~a & b & c
-// using the mux8 module from the previous exercise.
-
-module ex49 (input logic a, b, c
-             output logic y);
-    mux8 result({a, b, c}, 1, 0, 0, 1, 1, 1, 0, 0, y);
-endmodule
--- exercises/4/minority.sv
+++
@@ -1,7 +0,0 @@
-// Exercise 4.5
-// Write an HDL module called minority with three inputs. The output
-// should be true if at least two of the inputs are false.
-
-module minority (input logic a, b, c, output logic y);
-    assign y = a&b | b&c | a&c;
-endmodule
