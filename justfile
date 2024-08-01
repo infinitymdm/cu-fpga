680c3afa66ad5b3f2d8add6ca29a2af621b5e045
Marcus Mellor <infinitymdm@gmail.com>
2024-08-01 15:44:25 -0500

    Working tb_alu

--- justfile
+++ justfile
@@ -21,31 +21,31 @@ prep:
     mkdir -p {{build_dir}}
 
 # Synthesize the design
-synth design="main": prep
+synth design: prep
     yosys -q -p 'synth_ice40 -top {{design}} -json {{build_dir}}/{{dev_name}}_{{design}}.json' {{sources}}
 
 # Place and route the design
-pnr design="main": (synth design)
+pnr design: (synth design)
     nextpnr-ice40 --{{dev_family}} --package {{dev_model}} --json {{build_dir}}/{{dev_name}}_{{design}}.json --pcf {{constraints}} --asc {{build_dir}}/{{dev_name}}_{{design}}.asc
     icetime -d {{dev_family}} -mtr {{build_dir}}/{{dev_name}}_{{design}}.rpt {{build_dir}}/{{dev_name}}_{{design}}.asc
     icepack {{build_dir}}/{{dev_name}}_{{design}}.asc {{build_dir}}/{{dev_name}}_{{design}}.bin
 
 # Upload the design to the FPGA
-upload design="main": (pnr design)
+upload design: (pnr design)
     iceprog {{build_dir}}/{{dev_name}}_{{design}}.bin
 
 # Simulate the design against a testbench using verilator
-sim design="main": prep
-    verilator --trace --top {{design}} --cc {{sources}} --Mdir {{build_dir}} --exe {{tb_dir}}/tb_{{design}}.cpp
+sim design *FLAGS: prep
+    verilator --trace --x-assign unique --x-initial unique --top {{design}} --cc {{sources}} --Mdir {{build_dir}} --exe {{tb_dir}}/tb_{{design}}.cpp {{FLAGS}}
     make -C {{build_dir}} -f V{{design}}.mk V{{design}}
-    ./{{build_dir}}/V{{design}}
+    ./{{build_dir}}/V{{design}} +verilator+rand+reset+2
 
 # View simulation waveforms
 view:
     gtkwave {{wave_dir}}/*.vcd
 
 # Check the design for common code errors
-lint design="main":
+lint design:
     verilator --lint-only -Wall --top {{design}} -Isrc {{sources}}
 
 clean:
--- tb/tb_alu.cpp
+++ tb/tb_alu.cpp
@@ -1,30 +1,70 @@
 #include <stdlib.h>
+#include <climits>
 #include <iostream>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include <Valu.h>
 #include <Valu___024root.h>
 
-#define MAX_SIM_CYCLES 20
-vluint64_t sim_cycles = 0;
+// Wrapper functions for simulated versions of alu ops
+int sim_and (int x, int y) {return x & y;}
+int sim_or  (int x, int y) {return x | y;}
+int sim_xor (int x, int y) {return x ^ y;}
+int sim_add (int x, int y) {return x + y;}
+int sim_sub (int x, int y) {return x - y;}
+
+
+bool test_op (Valu *dut, vluint64_t &t_sim, VerilatedVcdC *m_trace, int op, int (*op_sim)(int, int)) {
+    int expected;
+
+    // Iterate over many values for x and y
+    dut->op_select = op;
+    for (int x = 0; x < 1000; x++) {
+        dut->a = x;
+        for (int y = 0; y < 1000; y++) {
+            dut->b = y;
+            dut->eval();
+
+            // Determine the expected result from the operator simulation function
+            expected = op_sim(x, y);
+
+            // Print an error and exit early if the result doesn't match what we expect
+            if (expected != dut->result) {
+                std::cout << "ERROR: result did not match expected"
+                    << "\nexpected: " << expected
+                    << "\nresult:   " << (int)(dut->result)
+                    << "\nt_sim:    " << t_sim << std::endl;
+                return false;
+            }
+
+            // Dump results to waveform and step sim time
+            m_trace->dump(t_sim);
+            t_sim++;
+        }
+    }
+    return true;
+}
+
 
 int main (int argc, char** argv, char** env) {
+
+    Verilated::commandArgs(argc, argv);
     Valu *dut = new Valu;
+    vluint64_t t_sim = 0;
 
     Verilated::traceEverOn(true);
     VerilatedVcdC *m_trace = new VerilatedVcdC;
     dut->trace(m_trace, 5);
-    m_trace->open("waveform.vcd");
+    m_trace->open("tb_alu.vcd");
 
-    dut->op_select = 8;
-    while (sim_cycles < MAX_SIM_CYCLES) {
-        dut->a++;
-        dut->eval();
-        m_trace->dump(sim_cycles);
-        sim_cycles++;
-    }
+    test_op(dut, t_sim, m_trace, 0, &sim_and);
+    test_op(dut, t_sim, m_trace, 1, &sim_or);
+    test_op(dut, t_sim, m_trace, 2, &sim_xor);
+    test_op(dut, t_sim, m_trace, 8, &sim_add);
+    test_op(dut, t_sim, m_trace, 9, &sim_sub);
 
     m_trace->close();
     delete dut;
+
     exit(EXIT_SUCCESS);
 }
