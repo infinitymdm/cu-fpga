e85d6f1b864fd8868096fded6d0da25d2c15a443
Marcus Mellor <infinitymdm@gmail.com>
2025-02-21 12:52:16 -0600

    organize: Move toplevels to dedicated folder

--- top/top_alu.sv
+++ top/top_alu.sv
@@ -1,9 +1,18 @@
+// top_alu.sv
+// Implements an 8-bit ALU with opcode, operands, and result displayed on LEDs.
+// Features:
+//  - Opcode displaed on Cu LEDs 3 through 0
+//  - Carry and zero flags displayed on Cu LEDs 5 and 4 respectively
+//  - Opcode and operands can be set using DIP switches
+//  - Operands a and b displayed on leftmost 7-segment displays as hex values
+//  - Result displayed on rightmost 7-segment displays as hex values
+
 module top_alu (
   input  logic       cu_clk,
   output logic [7:0] cu_led,
   input  logic       cu_btn_reset,
   output logic [7:0] io_led_a, io_led_b, io_led_c,
-  input  logic [7:0] io_dip_a, io_dip_b, 
+  input  logic [7:0] io_dip_a, io_dip_b,
   input  logic [3:0] io_dip_c,
   output logic [3:0] io_7seg_s,
   output logic [7:0] io_7seg
