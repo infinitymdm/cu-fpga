2113921ea44602b5686b8708c6acb0131e18523e
Marcus Mellor <infinitymdm@gmail.com>
2025-02-21 12:53:03 -0600

    keccak: Polish testbench, flesh out readme

--- tb/tb_sha3.sv
+++ tb/tb_sha3.sv
@@ -1,16 +1,31 @@
+`define TO_STRING(x) `"x`"
+
+// The following vars must be defined, usually in a simulator +define+ flag
+`ifndef DIGEST_LENGTH
+    `error_define_DIGEST_LENGTH_must_be_set
+`endif
+`ifndef STAGES
+    `error_define_STAGES_must_be_set
+`endif
+`ifndef MESSAGE_FILE
+    `error_define_MESSAGE_FILE_must_be_set
+`endif
+
 module tb_sha3;
 
-    localparam d = 512; // digest length. 384, 256, 224 all work
-    localparam s = 6; // stages. All integer divisors of 24 work
+    localparam d = `DIGEST_LENGTH;  // 512, 384, 256, 224 all work
+    localparam s = `STAGES;         // All integer divisors of 24 work
+    string message_file_name = {"../", `TO_STRING(`MESSAGE_FILE)};
+
+    // Derived
     localparam r = 1600 - 2*d;
 
-    string message_file_name = "../tb/sha3_test.bin";
     int message_file;
     int waited_cycles;
 
     bit clk, reset, enable;
     bit [r-1:0] message;
-    bit [d-1:0] digest;
+    bit [d-1:0] digest, expected_digest;
 
     keccak #(d, 6, s) dut (
         .clk, .reset, .enable,
@@ -33,10 +48,7 @@ module tb_sha3;
 
     initial begin: open_message_file
         message_file = $fopen(message_file_name, "rb");
-        if (message_file == 0) begin: handle_fopen_error
-            $display("Error: unable to open file '%s'", message_file_name);
-            $finish;
-        end
+        if (message_file == 0) $error("Unable to open file '%s'", message_file_name);
     end
 
     task automatic read_message_chunk (input int m_file, output bit [r-1:0] m);
@@ -62,21 +74,41 @@ module tb_sha3;
         end
     endtask
 
+    task automatic get_expected_digest (input string m_filename, output bit [d-1:0] expected_digest);
+        // Call openssl to get the expected result
+        int expected_file;
+        void'($system({"openssl dgst -sha3-", `TO_STRING(`DIGEST_LENGTH), " ", m_filename, " | awk '{print $2}' > .expected"}));
+        expected_file = $fopen(".expected", "r");
+        if (expected_file != 0) begin
+            void'($fscanf(expected_file, "%h", expected_digest));
+            $fclose(expected_file);
+        end else begin
+            $error("Unable to open file '.expected'");
+        end
+    endtask
+
     always @(posedge clk) begin: stimulate_dut
         if (!$feof(message_file)) begin: get_message
             reset = 1'b0;
             enable = 1'b1;
-            if (waited_cycles % s == 0) begin: read_chunk
-                read_message_chunk(message_file, message);
-                $display("Message chunk: %h", message);
-            end
+            if (waited_cycles % s == 0) read_message_chunk(message_file, message);
             waited_cycles++;
         end else begin: handle_eof
             #((s-1)*10);
             enable = 1'b0;
             #1;
-            $display("Result:        %h", digest);
+            $display("digest:  %h", digest);
             $fclose(message_file);
+            get_expected_digest(message_file_name, expected_digest);
+            $display("openssl: %h", expected_digest);
+            if (digest == expected_digest) begin
+                $write("%c[1;32m", 27); // Set style bold, green foreground
+                $display("PASS");
+            end else begin
+                $write("%c[1;30m", 27); // Set style bold, red foreground
+                $error("FAIL");
+            end
+            $write("%c[0m", 27); // reset graphics modes
             $finish;
         end
     end
